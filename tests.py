dd6ff99f9aef6810c1b4f892a739f2de14c86122
Brandon Tsao <brandonstsao@gmail.com>
2016-07-14 14:12:23 -0700

    Added DB persistence test, improved POST and GET tests to use status codes instead of stringified responses

--- tests.py
+++ tests.py
@@ -15,14 +15,23 @@ class AppTestCase(unittest.TestCase):
 
     def testGET(self):
         rv = self.app.get('/api/return/person/Testivus')
-        unittest.TestCase.assertEqual(self,str(rv),'<Response streamed [200 OK]>','GET Failed')
+        unittest.TestCase.assertEqual(self,rv.status_code,200)
 
         rv = self.app.get('/api/return/asset/Skynet')
-        unittest.TestCase.assertEqual(self,str(rv),'<Response streamed [200 OK]>','GET Failed')
+        unittest.TestCase.assertEqual(self,rv.status_code,200)
 
-    # def testDBPersistence(self):
-    #     rv = self.app.get('/api/return/person/Testivus')
-    #     print ('\n',type(rv))
+    def testDBPersistence(self):
+        rv = self.app.get('/api/return/person/Testivus')
+        datastring = rv.get_data(as_text=True)
+        unittest.TestCase.assertIn(self,'Testivus',datastring)
+
+        rv = self.app.get('/api/return/asset/Skynet')
+        datastring = rv.get_data(as_text=True)
+        unittest.TestCase.assertIn(self,'Skynet',datastring)
+
+
+
+
 
 
 

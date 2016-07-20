399ce88cfc6f1625d21d3782bc54f968853cd86e
Brandon Tsao <brandonstsao@gmail.com>
2016-07-20 16:13:41 -0700

    bug fixes

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ def returnAsset(asset):
 #=====#
 
 if __name__ == "__main__":
-    app.run(app.run(host='0.0.0.0', port=(int(os.environ.get('PORT', 33507)))))
+    app.run(host='0.0.0.0', port=(int(os.environ.get('PORT', 33507))))
